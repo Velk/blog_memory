163faa5a6f9d0e8a07e15a90d9b2d1dd8a7c7eb1
Julien Bonnanfant <julienbonnanfant@pc53.home>
2019-07-29 16:57:23 +0200

    first commit Mise en place du header (carousel+logo+navbar)

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const elements = document.querySelectorAll('.data li')
+const select = document.querySelector('#select_theme')
+
+select.addEventListener('change', () => {
+    elements.forEach( elem => {
+        const attr = elem.getAttribute('data-style');
+
+        if (attr === select.value || select.value === "*") {
+            elem.classList.remove('disable')
+        } else {
+            elem.classList.add('disable')
+        }
+    })
+})
